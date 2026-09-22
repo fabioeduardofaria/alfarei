33c25346fce203408a6802543efff61d4b17be4e
Fabio Eduardo Faria <fabioeduardofaria@gmail.com>
2026-09-21 20:03:59 -0400

    style: padroniza acabamento visual do sistema

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{{ asset('css/quotes.css') }}">
     <link rel="stylesheet" href="{{ asset('css/notifications.css') }}">
     <link rel="stylesheet" href="{{ asset('css/users.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/system-polish.css') }}">
 </head>
 <body>
 <div class="shell">
